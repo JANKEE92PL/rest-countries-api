de688ead151f7298b773f777e2ae814361b9570b
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-04-22 12:55:16 +0200

    creating seed file and test it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+10.times do
+  Country.create(
+    name: Faker::Address.country,
+    native_name: Faker::Address.country,
+    population: rand(1.0..100000000.00),
+    region: Faker::Address.state,
+    sub_region: Faker::Address.state_abbr,
+    capital: Faker::Address.city,
+    tld: Faker::Address.country_by_code,
+    currencies: Faker::Currency.name,
+    languages: Faker::ProgrammingLanguage.name,
+    border_countries: Faker::Address.country
+  )
+end
